b94e2adf06c37a58f57ee58869032961b80cf264
2191867E <jasonegan59@gmail.com>
2020-01-31 22:46:16 +0000

    refactored some code

--- dependencyFinder.py
+++ dependencyFinder.py
@@ -6,40 +6,33 @@ fileDepsMap = {}
 def obtainFiles(dir):
     return glob.glob(dir + "/*")
 
+
+def processLines(lines, filesToProcess, f):
+    for l in lines:
+        if "include" in l:
+            includeFile = l.split(" ")[1]
+            if not "<" in includeFile:
+                newDep = includeFile.replace("\"", "").replace("\n","")
+                if newDep not in fileDepsMap[f]:
+                    fileDepsMap[f].append(newDep)
+                    filesToProcess = [newDep] + filesToProcess
+    return filesToProcess
+
 def processFiles(fileList):
     filesToProcess = []
 
-    # this file breaks it
     for f in fileList:
-        print("on file " + f)
         if os.path.isfile(f):
+            f = f.replace("./Dataset/", "")
             if f not in fileDepsMap.keys():
                 fileDepsMap[f] = []
-            with open(f) as curFile:
-                lines = curFile.readlines()
-            for l in lines:
-                if "include" in l:
-                    includeFile = l.split(" ")[1]
-                    if not "<" in includeFile:
-                        newDep = includeFile.replace("\"", "").replace("\n","")
-                        filesToProcess = [newDep] + filesToProcess
-                        fileDepsMap[f].append(newDep)
+            filesToProcess = [f]
             while len(filesToProcess) != 0:
                 newFile = filesToProcess.pop()
                 if os.path.isfile('./Dataset/' + newFile):
                     with open('./Dataset/' + newFile) as curFile:
                         lines = curFile.readlines()
-                    for l in lines:
-                        if "include" in l:
-                            includeFile = l.split(" ")[1]
-                            if not "<" in includeFile:
-                                newDep = includeFile.replace("\"", "").replace("\n","")
-                                if newDep not in fileDepsMap[f]:
-                                    fileDepsMap[f].append(newDep)
-                                    filesToProcess = [newDep] + filesToProcess
-
-    print(fileDepsMap)
-
+                    filesToProcess = processLines(lines, filesToProcess, f)
 
 
 if __name__ == "__main__":
